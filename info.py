63cb8a8bf8d99c3cf19c63f5d9d5b143ed3d0190
Kokopw <kmelon175@gmail.com>
2025-02-25 01:04:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Strangerthing50')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CharlieSupportGroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Strangerthing50')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/CharlieSupportGroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/CharlieWatson1')
